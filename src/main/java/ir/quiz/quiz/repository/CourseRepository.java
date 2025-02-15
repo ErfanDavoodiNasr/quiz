c195b5235d9e17858c43ab9bb2d1c7c72ff4b38d
ErfanDavoodiNasr <davoodinasr.erfan@gmail.com>
2025-02-15 11:21:56 +0330

    add @Repository Annotation

--- src/main/java/ir/quiz/quiz/repository/CourseRepository.java
+++ src/main/java/ir/quiz/quiz/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package ir.quiz.quiz.repository;
 
 import ir.quiz.quiz.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
--- src/main/java/ir/quiz/quiz/repository/StudentRepository.java
+++ src/main/java/ir/quiz/quiz/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package ir.quiz.quiz.repository;
 
 import ir.quiz.quiz.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
