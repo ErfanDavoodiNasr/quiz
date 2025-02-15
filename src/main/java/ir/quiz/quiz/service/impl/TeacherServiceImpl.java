4d2604b86fc839684a84fb19e67758fdcb7748bc
ErfanDavoodiNasr <davoodinasr.erfan@gmail.com>
2025-02-15 11:40:58 +0330

    add service for models(course, principal, student, teacher)
    user dependency inversion and abstraction

---
+++ src/main/java/ir/quiz/quiz/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,8 @@
+package ir.quiz.quiz.service.impl;
+
+import ir.quiz.quiz.service.TeacherService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeacherServiceImpl implements TeacherService {
+}
